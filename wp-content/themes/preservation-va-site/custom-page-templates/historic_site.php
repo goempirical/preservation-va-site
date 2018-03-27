5c7f1836da1bfaf9f588cca54c16254679c34bce
Sebastian Pereyro <seba@goempirical.com>
2018-03-26 20:48:48 -0700

    adding custom page template historic site with acf

---
+++ wp-content/themes/preservation-va-site/custom-page-templates/historic_site.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template Name: Historic Site
+ */
+get_header();
+
+$container   = get_theme_mod( 'understrap_container_type' );
+
+?>
+
+<div class="wrapper" id="page-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<!-- Do the left sidebar check -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+
+			<main class="site-main" id="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php /*get_template_part( 'loop-templates/content', 'page' );*/ ?>
+
+          <h1>Title: <?php the_field('hs_title'); ?></h1>
+          <h4>Year of Origintation: <?php the_field('hs_year_of_origination'); ?></h4>
+          <h4>Address: <?php the_field('hs_address'); ?></h4>
+
+					<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+					?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+
+		</div><!-- #primary -->
+
+		<!-- Do the right sidebar check -->
+		<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+
+	</div><!-- .row -->
+
+</div><!-- Container end -->
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
