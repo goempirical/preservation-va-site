88fca6f39fc686cdf372e8669c79a3747b74a8f0
Juan Cardenas <jccdelghans@gmail.com>
2018-05-11 13:59:52 -0500

    Implement Theme Settings https://trello.com/c/js9kiUAD/80-implement-theme-settings

--- wp-content/themes/preservation-va-site/footer.php
+++ wp-content/themes/preservation-va-site/footer.php
@@ -15,11 +15,69 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="wrapper-footer">
 
-	<div class="<?php echo esc_attr( $container ); ?>">
+	<div class="<?php echo esc_attr( $container ); ?> footer">
 
 		<div class="row">
+			<div class="col-md-6">
+				<?php 
+				$opt_subscribe = get_field('opt_subscribe', 'option');
+				if ($opt_subscribe["opt_subscribe_snippet"]) {
+					echo $opt_subscribe["opt_subscribe_snippet"];
+				}
 
-			<div class="col-md-12">
+				$opt_footer = get_field('opt_footer', 'option'); 
+
+				if ($opt_footer["opt_footer_address"]) {
+					echo "<p class='address'>" . $opt_footer["opt_footer_address"] . "</p>";
+				}
+
+				echo "<p class='more_info'>";
+				if ($opt_footer["opt_footer_phone"]) {
+					echo "<span>phone " . $opt_footer["opt_footer_phone"] . "</span> &nbsp;| &nbsp;";
+				}
+
+				if ($opt_footer["opt_footer_fax"]) {
+					echo "<span>fax " . $opt_footer["opt_footer_fax"] . "</span> &nbsp;| &nbsp;";
+				}
+
+				if ($opt_footer["opt_footer_email"]) {
+					echo "<span>" . $opt_footer["opt_footer_email"] . "</span>";
+				}
+				echo "</p>";
+				?>
+				<p>&copy; <?php echo date('Y') ?> Preservation Virginia. All Rights Reserved.</p>
+			</div>
+			<div class="col-md-4 social_media">
+				<?php $opt_social = get_field('opt_social', 'option'); ?>
+				<?php if ($opt_social["opt_social_facebook"]) : ?>
+					<a href="<?php echo $opt_social['opt_social_facebook'] ?>" target="_blank">
+						<img src="<?php echo THEME_IMG_PATH ?>group-28@2x.png" width="35" />
+					</a>
+				<?php endif; ?>
+
+				<?php if ($opt_social["opt_social_twitter"]) : ?>
+					<a href="<?php echo $opt_social['opt_social_twitter'] ?>" target="_blank">
+						<img src="<?php echo THEME_IMG_PATH ?>group-27@2x.png" width="35" />
+					</a>
+				<?php endif; ?>
+
+				<?php if ($opt_social["opt_social_instagram"]) : ?>
+					<a href="<?php echo $opt_social['opt_social_instagram'] ?>" target="_blank">
+						<img src="<?php echo THEME_IMG_PATH ?>group-29@2x.png" width="35" />
+					</a>
+				<?php endif; ?>
+
+				<?php if ($opt_social["opt_social_youtube"]) : ?>
+					<a href="<?php echo $opt_social['opt_social_youtube'] ?>" target="_blank">
+						<img src="<?php echo THEME_IMG_PATH ?>group-31@2x.png" width="35" />
+					</a>
+				<?php endif; ?>
+				<div>
+					<a href="#" class="btn marigold small">CONTACT US</a>
+				</div>
+			</div>
+
+			<div class="col-md-2">
 
 				<footer class="site-footer" id="colophon">
 
