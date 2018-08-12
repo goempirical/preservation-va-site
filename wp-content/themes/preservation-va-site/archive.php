a724afb0027dbcf3c28fd213ccc6a55494674950
spencer8 <spencer@team-eight.com>
2018-08-12 17:53:35 -0400

    fixes events archives and related queries

--- wp-content/themes/preservation-va-site/archive.php
+++ wp-content/themes/preservation-va-site/archive.php
@@ -14,6 +14,20 @@ get_header();
 $container   = get_theme_mod( 'understrap_container_type' );
 ?>
 
+<section class="row headline-row">
+
+	<div class="col-md-12 no_padding_both_sides">
+
+		<div class="content_headline secondary_gray ">
+								
+			<?php the_archive_title( '<h2 class="page-title">', '</h2>' ); ?>
+															 
+		</div>
+
+	</div>
+
+</section>
+
 <div class="wrapper" id="archive-wrapper">
 
 	<div class="container" id="content" tabindex="-1">
@@ -27,24 +41,11 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 				<?php if ( have_posts() ) : ?>
 
-					<header class="page-header">
-						<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
-						?>
-					</header><!-- .page-header -->
-
 					<?php /* Start the Loop */ ?>
 					<?php while ( have_posts() ) : the_post(); ?>
 
 						<?php
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
+							get_template_part( 'loop-templates/content', get_post_type() );
 						?>
 
 					<?php endwhile; ?>
